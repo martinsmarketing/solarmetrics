12f0028b42e70522cacf561e65b3ff0f6566354a
Marty <marty@Martys-MacBook-Air.fritz.box>
2026-06-22 20:07:42 +0200

    Update site URL to getsolarmetrics.com for sitemap and canonical URLs

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://solarmetrics-nine.vercel.app',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://getsolarmetrics.com',
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: 'weekly',
